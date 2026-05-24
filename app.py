8f864deeef9f70d4bc82b6334269b2ade46f28d7
Yeron Dereje <yerondereje432@gmail.com>
2026-05-24 20:23:16 +0300

    Fix: init_db on startup

--- app.py
+++ app.py
@@ -532,6 +532,8 @@ def student_progress():
                            attendance=attendance)
 
 
+
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
